bfc8a874db1fea5ba69872c5e50f4ad339101e8d
Parvin Sattorova <nanajon66@gmail.com>
2023-10-29 11:44:57 -0600

    feature: Add article deteil components css file

--- src/Components/ArticleDetail/ArticleDetail.tsx
+++ src/Components/ArticleDetail/ArticleDetail.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { Article } from '../ArticleList/ArticleList'; 
+import '../ArticleDetail/ArticleDetail.css';
 
 interface ArticleDetailProps {
   articles: Article[];
@@ -18,7 +19,7 @@ const ArticleDetail: React.FC<ArticleDetailProps> = ({ articles }) => {
     <div className="article-detail-container">
       <Link to="/" className="back-button">Back to Main Page</Link>
       <h1>{article.title}</h1>
-      <img src={article.urlToImage || "path/to/default/image.jpg"} alt={article.title || "No Title"} />
+      <img src={article.urlToImage || "path/to/default/image.jpg"} alt={article.title || "No Title"} className="detail-image" />
       <p><strong>Source:</strong> {article.source.name}</p>
       <p><strong>Date:</strong> {new Date(article.publishedAt).toLocaleDateString()}</p>
       <p>{article.content}</p>
