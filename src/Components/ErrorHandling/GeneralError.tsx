d32d432c48e79b7dcd3d84222720c97778e66934
Parvin Sattorova <nanajon66@gmail.com>
2023-11-02 08:56:12 -0600

    refactor: Add edits to package.json file to specify a compatible TypeScript version

--- src/Components/ErrorHandling/GeneralError.tsx
+++ src/Components/ErrorHandling/GeneralError.tsx
@@ -1,26 +1,3 @@
-// import React from 'react';
-// import { NavLink } from 'react-router-dom';
-// import Superman from '../Images/superman.png';
-// import './GeneralError.css';
-
-// const GeneralError: React.FC = () => {
-//   return (
-//     <div className="general-error-msg">
-//       <h1>Oops! We're looking for it!</h1>
-//       <img src={Superman} alt="supperman" />
-//       <NavLink to="/" style={{ textDecoration: 'none' }}>
-//         <button className="general-error-go-home-button" aria-label="Go back to the home page">
-//           Back to Main Page
-//         </button>
-//       </NavLink>
-//       <h2 className="general-error-message">Something went wrong.</h2>
-//     </div>
-//   );
-// }
-
-// export default GeneralError;
-
-
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Superman from '../Images/superman.png';
